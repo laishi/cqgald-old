7308765eb85a839638c3a2574baa7862b2ec0703
laishi <laishi886@hotmail.com>
2016-10-28 12:53:50 +0800

    update grids

--- js/custom.js
+++ js/custom.js
@@ -57,137 +57,136 @@ $(document).ready(function() {
 
     var clickGrid ;
 
+
+
+
+
+
+
+
+
+
     var gridItem = $( ".gridItem" );
+
+
 
     gridItem.click(function(event) {
 
-        event.preventDefault();
 
-        var currentPX = $(this).position().left;
-
-        var wcX = window.innerWidth/2;
-        var wcY = window.innerHeight/2;
-
-
-        var pcX = $(this).parent().offset().left + $(this).parent().width()/2;
-        var pcY = $(this).parent().offset().top + $(this).parent().height()/2;
-
-        var ccX = $(this).offset().left + $(this).width()/2;
-        var ccY = $(this).offset().top + $(this).height()/2;
-
-        console.log(wcX +","+ wcY);
-        console.log(pcX +","+ pcY);
-        console.log(ccX +","+ ccY);
-
-
-
-
-
-
-
-
-
-
-
-
-        // TweenMax.to($(this),1, {x:wcX-ccX, y:wcY-ccY,scale:2});
-
-
-        // url = $(this).css('background-image').replace(/^url\(['"]?/,'').replace(/['"]?\)$/,'');
-        // $('.gridItems').css('background-image', 'url(' + url + ')');
-        // $('.gridItems').css('background-repeat', 'no-repeat');
-        // $('.gridItems').css('background-position', 'center');
-
-
-
-        // cache element in variable
-        var $img = $('.gridItems');
-        bgzoom = new TimelineMax({
-          //reversed:true,
-          paused:true,
-          // repeat:-1,
-          // yoyo:true,
-        });
-
-        // bgzoom.set($img,{backgroundSize:"0% 0%"}).to($img, 0.8, {backgroundSize: "auto 80%", ease: Power1.ease0ut}).play(); 
-
-
-
-
-        var DW = $( document ).width();
-
-        console.log(DW);
-
-        var count=0;
-        var gridPX;
-
-
-        var gridOBJL = [];
-        var gridOBJM = [];
-        var gridOBJR = [];
-
-
-
-        clickGrid = $(this);
-
-        var otherGridItem = $(this).siblings();
-        
-
-        $.each(otherGridItem,function(){
-            var gridPX = $(this).position().left;
-            // console.log(gridPX);
-            // console.log("index");
-            count++;
-
-
-            if (currentPX > gridPX) {
-
-                gridOBJL.push($(this));
-
-            }else {
-
-                gridOBJR.push($(this));
-            };
-
-
-        });
-
-        console.log(gridOBJL.length);
-
-
-
-        tl1 = new TimelineMax({
-          //reversed:true,
-          paused:true,
-          // repeat:-1,
-          // yoyo:true,
-        });
-
-        tl1.staggerTo(gridOBJL, 0.8, {x:-DW, opacity:0 }, 0.02 ).play(); 
-
-
-        tl2 = new TimelineMax({
-          //reversed:true,
-          paused:true,
-          // repeat:-1,
-          // yoyo:true,
-        });
-
-        tl2.staggerTo(gridOBJR, 0.8, {x:+DW, opacity:0 }, 0.02 ).play(); 
-
-
-
-        ctl = new TimelineMax({paused:true,});
-        ctl.to($(this),0.6, {x:wcX-ccX, y:wcY-ccY,scale:2.5,}).play()
-        // .to($(this),0.8, {scale:2.5,ease: Elastic.easeOut.config(1, 0.3)}).play();
-        $(this).toggleClass( "expandGrid" );
 
         if ($(this).hasClass( "expandGrid" )) {
 
 
+                tl1.reverse();
+                tl2.reverse();
+                ctl.reverse();
+
+
+            $(this).toggleClass( "expandGrid" );
+
+        } else{
+
+
+            var currentPX = $(this).position().left;
+
+            var wcX = window.innerWidth/2;
+            var wcY = window.innerHeight/2;
+
+
+            var pcX = $(this).parent().offset().left + $(this).parent().width()/2;
+            var pcY = $(this).parent().offset().top + $(this).parent().height()/2;
+
+            var ccX = $(this).offset().left + $(this).width()/2;
+            var ccY = $(this).offset().top + $(this).height()/2;
+
+            console.log(wcX +","+ wcY);
+            console.log(pcX +","+ pcY);
+            console.log(ccX +","+ ccY);
+
+            var $img = $('.gridItems');
+            bgzoom = new TimelineMax({
+              //reversed:true,
+              paused:true,
+              // repeat:-1,
+              // yoyo:true,
+            });
+
+            // bgzoom.set($img,{backgroundSize:"0% 0%"}).to($img, 0.8, {backgroundSize: "auto 80%", ease: Power1.ease0ut}).play(); 
+
+
+
+
+            var DW = $( document ).width();
+
+            console.log(DW);
+
+            var count=0;
+            var gridPX;
+
+
+            var gridOBJL = [];
+            var gridOBJM = [];
+            var gridOBJR = [];
+
+
+
+            clickGrid = $(this);
+
+            var otherGridItem = $(this).siblings();
+            
+
+            $.each(otherGridItem,function(){
+                var gridPX = $(this).position().left;
+                // console.log(gridPX);
+                // console.log("index");
+                count++;
+
+
+                if (currentPX > gridPX) {
+
+                    gridOBJL.push($(this));
+
+                } else {
+
+                    gridOBJR.push($(this));
+                };
+
+
+            });
+
+            console.log(gridOBJL.length);
+
+
+
+            tl1 = new TimelineMax({
+              //reversed:true,
+              paused:true,
+              // repeat:-1,
+              // yoyo:true,
+            });
+
+            tl1.staggerTo(gridOBJL, 0.38, {x:-DW, opacity:0, scale:0.1 }, 0.02 ).play(); 
+
+
+            tl2 = new TimelineMax({
+              //reversed:true,
+              paused:true,
+              // repeat:-1,
+              // yoyo:true,
+            });
+
+            tl2.staggerTo(gridOBJR, 0.38, {x:+DW, opacity:0, scale:0.1 }, 0.02 ).play(); 
+
+
+
+            ctl = new TimelineMax({paused:true,});
+            ctl.to($(this),0.5, {x:wcX-ccX, y:wcY-ccY,scale:2.5,}).play()
+            // .to($(this),0.8, {scale:2.5,ease: Elastic.easeOut.config(1, 0.3)}).play();
+
+
+            $(this).toggleClass( "expandGrid" );
 
         }
-
 
 
 
@@ -196,12 +195,6 @@ $(document).ready(function() {
 
 
 
-    $(".fz").click(function (event) {
-
-        tl1.reverse();
-        tl2.reverse();
-        ctl.reverse();
-    })
 
 
     // TweenMax.fromTo([element1, element2], 1, {x:0}, {x:100});
